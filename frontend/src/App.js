f87350b7eb78ffa9b211358116fdfd5ad6210243
pothirendirahul <pothirendi.r@northeastern.edu>
2026-04-14 11:33:52 -0400

    minor-fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useMemo, useState } from "react";
 import axios from "axios";
-const API_BASE =
-  process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000";
 import {
   BarChart,
   Bar,
@@ -17,6 +15,9 @@ import {
   Legend,
 } from "recharts";
 import "./App.css";
+
+const API_BASE =
+  process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000";
 
 function StatCard({ title, value, subtitle }) {
   return (
